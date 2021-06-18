22806934c69a44378e1e6de3a312d26b7a7b7c3a
Anthony Catullo <acatullo4@gmail.com>
2021-06-18 18:23:53 -0500

    added oop code for home and comment classes

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,5 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     HomeApi.fetchHomes()
     CommentApi.fetchComments()
 
+    const homeForm = document.getElementById("home-form")
+    const homeInput = document.getElementById("home-input")
+    const homeList = document.getElementById("home-list")
+    const homeURL = `http://localhost:3000/home`
+    const commentURL = `http://localhost:3000/comments`
+
+    homeForm.addEventListener("submit", Home.submitHome)
+
+    Home.fetchHome()
+
+
+
 })
 
--- javascripts/models/home.js
+++ javascripts/models/home.js
@@ -10,4 +10,88 @@ class Home {
         Home.all.push(this) 
     }
 
+    static renderHomes(){
+        for(let home of this.allHomes){
+            home.renderHome()
+        }
+    }
+
+    static fetchHomes(){
+        fetch(homeURL)
+        .then(res => res.json())
+        .then(homes => {
+            for(let home of homes){
+                let newList = new Home(home.data)
+            }
+            this.renderHomes()
+        })
+    }
+
+    renderHome(){
+        const li = document.createElement('li')
+        li.dataset.id = this.id
+    
+        const p = document.createElement('p')
+        p.innerText = this.address
+    
+        const deleteBtn = document.createElement("button")
+        deleteBtn.innerText = "delete"
+        deleteBtn.addEventListener("click", this.deleteHome)
+    
+        const commentForm = document.createElement('form')
+        commentForm.innerHTML += `<input type="text" id="comment-input"><input type="submit">`
+        commentForm.addEventListener("submit", Comment.createComment)
+    
+        const commentList = document.createElement('ul')
+        this.comments.forEach(comment => {
+            let commentObj = new Comment(comment)
+            console.log(commentObj)
+            commentObj.renderComment(commentList)
+        })
+    
+        li.append(p, deleteBtn, commentForm, commentList)
+    
+        homeList.appendChild(li)
+        
+        homeForm.reset()
+    }
+
+    static submitHome(){
+        event.preventDefault()
+        const configObj = {
+            method: "POST", 
+            headers: {
+                "Content-type": "application/json", 
+                "Accept": "application/json"
+            }, 
+            body: JSON.stringify({
+                address: homeInput.value
+            })
+        }
+    
+        fetch(homeURL, configObj)
+        .then(res => res.json())
+        .then(data => {
+            let newHome = new Home(data.data)
+            newHome.renderHome()
+        })
+    
+    }
+
+
+    deleteHome(){
+        const homeId = this.parentElement.dataset.id
+    
+        fetch(`${homeURL}/${homeId}`, {
+            method: "DELETE"
+        })
+    
+        this.parentElement.remove()
+    }
+
+
+    
+
+
 }
+
