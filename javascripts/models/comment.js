1395e4184569f2a8f99e6d7ac7a5d1baf168f52d
Anthony Catullo <acatullo4@gmail.com>
2021-06-22 16:31:58 -0500

    update

--- javascripts/models/comment.js
+++ javascripts/models/comment.js
@@ -2,24 +2,24 @@ class Comment {
 
     static all = []
 
-    constructor({id, content, home}){
-        this.id = id 
-        this.content = content 
+    constructor({ id, content, home }) {
+        this.id = id
+        this.content = content
         this.homeId = home.id
         Comment.all.push(this)
     }
 
-    static findCommentById(commentId){
+    static findCommentById(commentId) {
         return Comment.all.find(comment => comment.id === commentId)
     }
 
-    render(){
-        if (document.querySelector(`#comment-${this.id}`)){
+    render() {
+        if (document.querySelector(`#comment-${this.id}`)) {
             return
         }
         const homeDiv = document.getElementById(`home-${this.homeId}`)
         const div = document.createElement('div')
-        
+
         div.id = `comment-${this.id}`
         div.classList.add('comment')
         div.innerHTML = `
@@ -28,12 +28,12 @@ class Comment {
 
         const deleteButton = document.createElement('button')
         deleteButton.classList.add('delete-comment-button')
-        deleteButton.dataset.commentId = this.id 
+        deleteButton.dataset.commentId = this.id
         deleteButton.innerText = "Delete Comment"
 
         const editButton = document.createElement('button')
         editButton.classList.add('edit-comment-button')
-        editButton.dataset.commentId = this.id 
+        editButton.dataset.commentId = this.id
         editButton.innerText = "Edit Comment"
 
         div.appendChild(editButton)
@@ -44,18 +44,18 @@ class Comment {
         editButton.addEventListener('click', Comment.editComment)
     }
 
-    static editComment(event){
+    static editComment(event) {
         const commentId = event.target.dataset.commentId
-        const comment =  Comment.findCommentById(parseInt(commentId))
+        const comment = Comment.findCommentById(parseInt(commentId))
         comment.renderCommentEditForm()
     }
 
-    renderCommentEditForm(){
+    renderCommentEditForm() {
         const container = document.getElementById(`comment-${this.id}`)
         const commentInfo = container.querySelector('.comment-info')
-       
+
         commentInfo.classList.add('d-none')
-        
+
         const form = `
          <form data-comment-id='${this.id}' class="edit-comment-form">
              <label for="content">Content</label>
@@ -64,40 +64,40 @@ class Comment {
          </form>
          <br>
         `
- 
+
         const formContainer = document.createElement('div')
         formContainer.classList.add('edit-form-container')
         formContainer.innerHTML = form
         container.prepend(formContainer)
         container.querySelector('.edit-comment-button').classList.add('d-none')
         container.querySelector('.edit-comment-form').addEventListener('submit', CommentApi.submitCommentEdit)
-     }
+    }
 
-    static handleViewCommentClick(e){
+    static handleViewCommentClick(e) {
         const homeId = e.target.dataset.homeId
         CommentApi.fetch(homeId)
         const button = document.createElement('button')
         button.innerText = "Add Comment"
         button.classList.add('add-comment-button')
         button.dataset.homeId = homeId
-        
+
         const homeDiv = document.querySelector(`#home-${homeId}`)
         homeDiv.appendChild(button)
-        
+
         button.addEventListener('click', Comment.newComment)
-        
+
     }
 
-    static newComment(event){
-        const commentId = event.target.dataset.commentId
-        const comment =  Comment.findCommentById(parseInt(commentId))
+    static newComment(event) {
+        const homeId = event.target.dataset.homeId
+        const comment = Comment.findCommentById(parseInt(homeId))
         comment.renderNewCommentForm()
     }
 
-    renderNewCommentForm(){
+    renderNewCommentForm() {
         const container = document.getElementById(`comment-${this.id}`)
         const commentInfo = container.querySelector('.comment-info')
- 
+
         const form = `
          <form data-comment-id='${this.id}' class="new-comment-form">
              <label for="content">Content</label>
@@ -106,22 +106,39 @@ class Comment {
          </form>
          <br>
         `
-    
+
         const formContainer = document.createElement('div')
         formContainer.classList.add('new-comment-form')
         formContainer.innerHTML = form
         container.append(formContainer)
         container.querySelector('.add-comment-button').classList.add('d-none')
         container.querySelector('.new-comment-form').addEventListener('submit', CommentApi.submitComment)
+        
+        container.querySelector('.new-comment-form').addEventListener('submit', Comment.renderNewComment)
     }
 
-    reRender(){
-        if (document.querySelector(`#comment-${this.id}`)){
+    renderNewComment(event) {
+        if (document.querySelector(`#comment-${this.id}`)) {
             return
         }
         const homeDiv = document.getElementById(`home-${this.homeId}`)
         const div = document.createElement('div')
-        
+
+        div.id = `comment-${this.id}`
+        div.classList.add('comment')
+        div.innerHTML = `
+            <li class="comment-info">${this.content}</li>
+        `
+        homeDiv.appendChild(div)
+    }
+
+    reRender() {
+        if (document.querySelector(`#comment-${this.id}`)) {
+            return
+        }
+        const homeDiv = document.getElementById(`home-${this.homeId}`)
+        const div = document.createElement('div')
+
         div.id = `comment-${this.id}`
         div.classList.add('comment')
         div.innerHTML = `
@@ -130,12 +147,12 @@ class Comment {
 
         const deleteButton = document.createElement('button')
         deleteButton.classList.add('delete-comment-button')
-        deleteButton.dataset.commentId = this.id 
+        deleteButton.dataset.commentId = this.id
         deleteButton.innerText = "Delete Comment"
 
         const editButton = document.createElement('button')
         editButton.classList.add('edit-comment-button')
-        editButton.dataset.commentId = this.id 
+        editButton.dataset.commentId = this.id
         editButton.innerText = "Edit Comment"
 
         div.appendChild(editButton)
@@ -148,6 +165,6 @@ class Comment {
 
 
 
-  
+
 
 }
